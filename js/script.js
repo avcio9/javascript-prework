5220f914a64f0355af2162efa3fa55c71533f10d
Avcio <79534197+avcio9@users.noreply.github.com>
2023-06-02 11:13:27 +0200

    changed declarations

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-let buttonPaper, buttonRock, buttonScissors;
-// tu będą wymienione pozostałe zmienne guzików, np. buttonRock
-
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
+let buttonRock = document.getElementById('button-rock'),
+buttonPaper = document.getElementById('button-paper'),
 buttonScissors = document.getElementById('button-scissors');
-
-// tu będą kolejne linie kodu, w których do zmiennych będą przypisane elementy znalezione po id
-
-function buttonClicked(argButtonName) {
-    clearMessages();
-    printMessage(displayResult(argButtonName));
-}
 
 buttonRock.addEventListener('click', function () {
     buttonClicked('kamień')
@@ -21,3 +11,8 @@ buttonPaper.addEventListener('click', function () {
 buttonScissors.addEventListener('click', function () {
     buttonClicked('nożyce')
 });
+
+function buttonClicked(argButtonName) {
+    clearMessages();
+    printMessage(displayResult(argButtonName));
+}
